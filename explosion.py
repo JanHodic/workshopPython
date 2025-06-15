5b6c9fea9c12a76157c0c73dac440edaa8e6db00
Patrik Sekerka <patrik.sekerka@czso.cz>
2025-06-15 19:27:05 +0200

    Update explosion.py

--- explosion.py
+++ explosion.py
@@ -4,12 +4,12 @@ class Explosion(pygame.sprite.Sprite):
     def __init__(self, x, y, clock):
         super().__init__()
         self.sprite_sheet = pygame.image.load("assets/boom.png").convert_alpha()
-        self.frame_width = self.sprite_sheet.get_width() // 7  # 7 snímků
+        self.frame_width = self.sprite_sheet.get_width() // 5  # 7 snímků
         self.frame_height = self.sprite_sheet.get_height()
         self.clock = clock
         self.frames = [
             self.sprite_sheet.subsurface(pygame.Rect(i * self.frame_width, 0, self.frame_width, self.frame_height))
-            for i in range(7)
+            for i in range(5)
         ]
 
         self.frame_index = 0
